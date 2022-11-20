c1a1acbabfdc857e343515986cf6659a368221d3
RETprojects <77463120+RETprojects@users.noreply.github.com>
2022-11-20 17:06:44 -0600

    Add patterns_portal_spider.py
    This Scrapy spider crawls all of the design pattern pages on Refactoring Guru and stores the contents of each page in a HTML file. It works by recursively following the link to the next page.

---
+++ patterns_portal_spider.py
@@ -0,0 +1,23 @@
+# Remi T, 11/17/22, 11:43 am central time
+# This spider will be used to crawl all design patterns on the design pattern catalog page of Refactoring Guru.
+import scrapy
+
+
+class PatternsPortalSpider(scrapy.Spider):
+    name = "portal"
+    
+    # The starting URLs are derived from the patterns featured on the main design patterns portal.
+    start_urls = [
+        'https://refactoring.guru/design-patterns/creational-patterns',
+    ]
+
+    def parse(self, response):
+        page = response.url[response.url.rfind("/") + 1:]
+        filename = f'patterns-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
+
+        next_page = response.css('div.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
