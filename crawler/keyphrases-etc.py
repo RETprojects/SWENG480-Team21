ff3f60d8196b3f596b5ec6c6e72d5f662ca0909e
remi <remitoutin@gmail.com>
2022-11-21 14:44:14 -0600

    I tested how keyword extraction would work without as many example-oriented words in the input text.

--- crawler/keyphrases-etc.py
+++ crawler/keyphrases-etc.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from wordcloud import WordCloud, STOPWORDS
 
+"""
 text = "Imagine that you’re creating a logistics management application. " \
        "The first version of your app can only handle transportation by " \
        "trucks, so the bulk of your code lives inside the Truck class. " \
@@ -33,6 +34,24 @@ text = "Imagine that you’re creating a logistics management application. " \
        "need to make all of these changes again. As a result, you will end " \
        "up with pretty nasty code, riddled with conditionals that switch " \
        "the app’s behavior depending on the class of transportation objects."
+"""
+# the same text without as many example-oriented words
+text = "Imagine that you’re creating a  application. " \
+       "The first version of your app can only handle  by " \
+       ", so the bulk of your code lives inside the  class. " \
+       "After a while, your app becomes  popular. Each day you " \
+       "receive dozens of requests  to " \
+       "incorporate  into the app. Adding a new " \
+       " class to the program causes an issue. Adding a new " \
+       "class to the program isn’t that simple if the rest of the code is " \
+       "already coupled to existing classes. Great news, right? But how " \
+       "about the code? At present, most of your code is coupled to the " \
+       " class. Adding  into the app would require making " \
+       "changes to the entire codebase. Moreover, if later you decide to " \
+       "add another type of  to the app, you will probably " \
+       "need to make all of these changes again. As a result, you will end " \
+       "up with  nasty code, riddled with conditionals that switch " \
+       "the app’s behavior depending on the class of  objects."
 
 # using NLTK
 english_stops = set(stopwords.words('english'))
