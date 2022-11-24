7d8050c76741ae4add5ed778637584bff9068e61
remi <remitoutin@gmail.com>
2022-11-23 23:36:35 -0600

    I set up a blueprint for a file to handle determining the best pattern class for a design problem.

---
+++ ml/find_problem_category.py
@@ -0,0 +1,26 @@
+# Remi T, created 11/23/22 11:31 pm CST
+# This file contains code to determine the most appropriate design pattern
+# category for a design problem.
+
+# Sources:
+#   S. Hussain, J. Keung, M. K. Sohail, A. A. Khan, and M. Ilahi, “Automated
+#       Framework for classification and selection of software design
+#       patterns,” Applied Soft Computing, vol. 75, pp. 1–20, Feb. 2019.
+
+# imports, etc.
+
+
+# remove stop words
+
+# word stemming
+
+# indexing activities to create the VSM for the design patterns and unique
+# words
+
+
+# apply the weighting methods
+
+
+# assess the unsupervised learners w/ their best weighting methods (previously
+# determined) to determine the appropriate pattern class for the problem
+
