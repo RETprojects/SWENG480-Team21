23a4a1934b6536cfced9309e94ef37f1b50f95f0
remi <remitoutin@gmail.com>
2022-11-23 23:27:47 -0600

    I set up a blueprint for a file to handle design pattern organization from a catalog.

---
+++ ml/pattern_organization.py
@@ -0,0 +1,27 @@
+# Remi T, created 11/23/22 11:17 pm CST
+# This file contains code to organize design patterns.
+
+# Sources:
+#   S. Hussain, J. Keung, M. K. Sohail, A. A. Khan, and M. Ilahi, “Automated
+#       Framework for classification and selection of software design
+#       patterns,” Applied Soft Computing, vol. 75, pp. 1–20, Feb. 2019.
+
+# imports, etc.
+
+
+# remove stop words
+
+# word stemming
+
+# indexing activities to create the vector space model (VSM) for the N design
+# patterns and M unique words
+
+
+# apply the weighting methods
+
+
+# assess the performance of each unsupervised learner w/ all weighting methods
+
+
+# assess the performance of unsupervised learners w/ their finest weighting method
+
