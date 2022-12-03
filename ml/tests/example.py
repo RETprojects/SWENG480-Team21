ec921cbc5ed9ad4617663fffb430f3c8a72777c1
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2022-12-02 20:58:59 -0800

    Add example unit test

---
+++ ml/tests/example.py
@@ -0,0 +1,22 @@
+import unittest
+
+# (venv) jonathangarelick@Jonathans-MacBook-Pro SWENG480-Team21 % python -m unittest -v ml/tests/example.py
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
