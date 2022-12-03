976ecf2fe289bba62f0b7bf989a7fc953887317e
RETprojects <77463120+RETprojects@users.noreply.github.com>
2022-12-03 12:30:03 -0600

    Update createTables.sql

--- database/createTables.sql
+++ database/createTables.sql
@@ -9,8 +9,8 @@ CREATE TABLE pattern_catalog
 
 CREATE TABLE pattern_category
 (
-  id INT(10) unsigned NOT NULL AUTO_INCREMENT,
-  catalog_id INT(5) unsigned NOT NULL,	
+  id INT(10) NOT NULL AUTO_INCREMENT,
+  catalog_id INT(5) NOT NULL,	
   name VARCHAR(150) NOT NULL,
   description VARCHAR(300) NOT NULL,
   PRIMARY KEY (id),
@@ -19,8 +19,8 @@ CREATE TABLE pattern_category
 
 CREATE TABLE pattern
 (
-  id INT(20) unsigned NOT NULL AUTO_INCREMENT,
-  category_id INT(10) unsigned NOT NULL,
+  id INT(20) NOT NULL AUTO_INCREMENT,
+  category_id INT(10) NOT NULL,
   name VARCHAR(150) NOT NULL,
   intent VARCHAR(150) NOT NULL,
   motivation VARCHAR(150) NOT NULL,
@@ -31,25 +31,25 @@ CREATE TABLE pattern
   implementation VARCHAR(150) NOT NULL,
   related_patterns VARCHAR(150) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (category_id)
+  FOREIGN KEY (category_id) REFERENCES pattern_category(id)
 );
 
 CREATE TABLE problem
 (
-  id			INT(50) unsigned NOT NULL AUTO_INCREMENT, # Unique ID for the record
-  category_id	INT(10) unsigned NOT NULL,				  # ID for the pattern category to which the problem belongs
-  description	VARCHAR(150) NOT NULL,                # Description of the problem
-  PRIMARY KEY	(id),                                 # Make the id the primary key
-  FOREIGN KEY	(category_id)							  # Make the category id a foreign key
+  id INT(50) NOT NULL AUTO_INCREMENT,
+  category_id INT(10) NOT NULL,
+  description VARCHAR(150) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (category_id) REFERENCES pattern_category(id)
 );
 
 CREATE TABLE problem_pattern_match
 (
-  id					INT(200) unsigned NOT NULL AUTO_INCREMENT, # Unique ID for the record
-  problem_id			INT(50) unsigned NOT NULL,				  # ID for the problem that has been matched with a pattern
-  pattern_id			INT(20) unsigned NOT NULL,				  # ID for the pattern that has been matched with a problem
-  similarity_score	DOUBLE(23),							  # Similarity score of the pattern for the problem
-  PRIMARY KEY	(id),                                  		  # Make the id the primary key
-  FOREIGN KEY	(problem_id),									  # Make the problem id a foreign key
-  FOREIGN KEY	(pattern_id)									  # Make the pattern id another foreign key
+  id INT(200) NOT NULL AUTO_INCREMENT,
+  problem_id INT(50) NOT NULL,
+  pattern_id INT(20) NOT NULL,
+  similarity_score DOUBLE(23),
+  PRIMARY KEY (id),
+  FOREIGN KEY (problem_id) REFERENCES problem(id),
+  FOREIGN KEY (pattern_id) REFERENCES pattern(id)
 );
