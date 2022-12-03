fe02ffca0184fd264a67e01bebb4a0876dee1158
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2022-12-03 10:06:08 -0800

    Add CSV example

--- crawler/tutorial/tutorial/spiders/example.py
+++ crawler/tutorial/tutorial/spiders/example.py
@@ -1,4 +1,12 @@
 import scrapy
+from scrapy.item import Item, Field
+
+
+# scrapy crawl example -O myfile.csv:csv
+class CustomItem(Item):
+    header = Field()
+    paragraph = Field()
+    link = Field()
 
 
 class ExampleSpider(scrapy.Spider):
@@ -7,4 +15,5 @@ class ExampleSpider(scrapy.Spider):
     start_urls = ['http://example.com/']
 
     def parse(self, response):
-        pass
+        text = [s for s in response.css('div ::text').getall() if s.strip()]
+        return CustomItem(header=text[0], paragraph=text[1], link=text[2])
