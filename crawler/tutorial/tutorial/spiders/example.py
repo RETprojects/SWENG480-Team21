27c8a5653e20fb0499611c106f47f0ebe2710bd3
RETprojects <77463120+RETprojects@users.noreply.github.com>
2022-12-04 16:07:36 -0600

    Update example.py
    Uncommented some lines related to the Python MySQL connector.

--- crawler/tutorial/tutorial/spiders/example.py
+++ crawler/tutorial/tutorial/spiders/example.py
@@ -14,16 +14,16 @@ class CustomItem(Item):
 
 
 class ExampleSpider(scrapy.Spider):
-    # HOST = "root"
-    # # database name, if you want just to connect to MySQL server, leave it empty
-    # DATABASE = "pattern_recommender"
-    # # this is the user you create
-    # USER = ""
-    # # user password
-    # PASSWORD = "THXfortheF1$h!"
-    # # connect to MySQL server
-    # db_connection = mysql.connector.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
-    # print("Connected to:", db_connection.get_server_info())
+    HOST = "root"
+    # database name, if you want just to connect to MySQL server, leave it empty
+    DATABASE = "pattern_recommender"
+    # this is the user you create
+    USER = ""
+    # user password
+    PASSWORD = "THXfortheF1$h!"
+    # connect to MySQL server
+    db_connection = mysql.connector.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+    print("Connected to:", db_connection.get_server_info())
 
     name = 'example'
     #allowed_domains = ['example.com']
