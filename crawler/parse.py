7c4af36c29eb01dbea62a1c9f1287a8f3e7c9fed
Benjamin <bmk5670@psu.edu>
2022-12-06 14:23:12 -0500

    Updated

--- crawler/parse.py
+++ crawler/parse.py
@@ -5,25 +5,17 @@ import os.path
 class NewHTMLParser(HTMLParser):
     text = ""
 
-    # def handle_starttag(self, tag, attrs):
-    #     if tag == "h3":
-    #         print("Start tag:", tag)
-
-    # def handle_endtag(self, tag):
-    #     if tag == "h3":
-    #         print("End tag:", tag)
-
     def handle_data(self, data):
         self.text += data
 
 
-output = ""
 parser = NewHTMLParser()
 d = os.getcwd() + r"\tutorial\patterns"
 for (path) in os.listdir(d):
     file = open(d + "\\" + path, "r", encoding='utf-8')
+    file2 = open(os.getcwd() + r"\parsed_patterns\object_oriented\\" + path[0:path.find(".html")] + ".txt", "w", encoding='utf-8')
     parser.feed(file.read())
-    output += parser.text[parser.text.find("Intent"):parser.text.find("Support our free website and own the eBook!")]
+    file2.write(parser.text[parser.text.find("Intent"):parser.text.find("Support our free website and own the eBook!")])
     parser.text = ""
     file.close()
-print(output)
+# print(output)
