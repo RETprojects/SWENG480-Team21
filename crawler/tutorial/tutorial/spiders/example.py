d03830443b34eb39894af146fd73d18543a806b1
remi <remitoutin@gmail.com>
2022-12-05 22:12:47 -0600

    Corrected SQL statements to insert a new row into the pattern_ML table

--- crawler/tutorial/tutorial/spiders/example.py
+++ crawler/tutorial/tutorial/spiders/example.py
@@ -14,16 +14,7 @@ class CustomItem(Item):
 
 
 class ExampleSpider(scrapy.Spider):
-    HOST = "localhost"
-    # database name, if you want just to connect to MySQL server, leave it empty
-    DATABASE = "pattern_recommender"
-    # this is the user you create
-    USER = "root"
-    # user password
-    PASSWORD = "THXfortheF1$h!"
-    # connect to MySQL server
-    db_connection = mysql.connector.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
-    print("Connected to:", db_connection.get_server_info())
+
 
     name = 'example'
     #allowed_domains = ['example.com']
@@ -31,12 +22,33 @@ class ExampleSpider(scrapy.Spider):
 
 
     def parse(self, response):
+        HOST = "localhost"
+        # database name, if you want just to connect to MySQL server, leave it empty
+        DATABASE = "pattern_recommender"
+        # this is the user you create
+        USER = "root"
+        # user password
+        PASSWORD = "THX4theF1$h!"
+        # connect to MySQL server
+        db_connection = mysql.connector.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+        print("Connected to:", db_connection.get_server_info())
+
+        cursor = db_connection.cursor()
+
         file = open('temp_text.txt', 'w', encoding="utf-8")
         soup = BeautifulSoup(response.text, 'lxml')
+
+        last_heading = ""
+        intent = ""
+        problem = ""
+        discussion = ""
+        structure = ""
 
         for s in soup.select('h3'):
             if s.text == "Example":
                 break
+
+            last_heading = s.text
 
             file.write(s.text + '\n\n')
 
@@ -45,6 +57,22 @@ class ExampleSpider(scrapy.Spider):
                     file.write('\n\n')
                     break
 
+                if last_heading == "Intent":
+                    # write the current text to the intent field
+                    intent += ns.text
+                elif last_heading == "Problem":
+                    problem += ns.text
+                elif last_heading == "Discussion":
+                    discussion += ns.text
+                else:# last_heading == "Structure":
+                    structure += ns.text
                 file.write(ns.text + '\n')
                 print(ns)
 
+        # insert the pieces of text collected into a new DB entry
+        add_pattern = ("INSERT INTO pattern_ML "
+                       "(category_id, name, intent, problem, discussion, structure) "
+                       "VALUES (%s, %s, %s, %s, %s, %s)")
+        data_pattern = (0, 'Abstract Factory', intent, problem, discussion, structure)
+        cursor.execute(add_pattern, data_pattern) # insert new pattern
+        db_connection.commit() # commit the data to the DB
