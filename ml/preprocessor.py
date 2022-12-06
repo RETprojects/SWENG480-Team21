d191a077e5736c7af622b673e0f0a975c363c635
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2022-12-06 10:13:34 -0800

    Add preprocessor tests

--- ml/preprocessor.py
+++ ml/preprocessor.py
@@ -6,24 +6,23 @@ import string
 class Preprocessor:
     stemmer = PorterStemmer()
 
-
     @staticmethod
-    def remove_punctuation(input):
+    def remove_punctuation(input_str):
         """Remove punctuation from a string.
 
         Keyword arguments:
         input -- a string of any length
         """
-        return input.translate(str.maketrans('', '', string.punctuation))
+        return input_str.translate(str.maketrans('', '', string.punctuation))
 
     @staticmethod
-    def remove_stop_words(input):
+    def remove_stop_words(input_str):
         """Remove stop words from a string.
 
         Keyword arguments:
         input -- a string of any length
         """
-        return [word for word in input.split(' ') if word not in stopwords.words('english')]
+        return [word.strip() for word in input_str.split(' ') if word and word not in stopwords.words('english')]
 
     @classmethod
     def do_stem(cls, word_list):
@@ -32,13 +31,4 @@ class Preprocessor:
         Keyword arguments:
         input -- an array of strings
         """
-        return [cls.stemmer.stem(word) for word in word_list]
-
-
-my_str = "Today I went to the store to buy some groceries. I bought carrots, apples, and bananas."
-no_punc = Preprocessor.remove_punctuation(my_str)
-no_stop_words = Preprocessor.remove_stop_words(no_punc)
-stemmed = Preprocessor.do_stem(no_stop_words)
-print(f'No punctuation: {no_punc}\nAnd no stop words: {no_stop_words}')
-print(f'Stemmed: {stemmed}')
-
+        return [cls.stemmer.stem(word.strip()) for word in word_list if word]
--- ml/tests/test_preprocessor.py
+++ ml/tests/test_preprocessor.py
@@ -1,23 +1,70 @@
 import unittest
 
+from ml.preprocessor import Preprocessor
+
 
 # (venv) jonathangarelick@Jonathans-MacBook-Pro SWENG480-Team21 % python -m unittest -v ml/tests/test_preprocessor.py
 
 class TestPreprocessor(unittest.TestCase):
+    def setUp(self):
+        self.str_simple = "Today I went to the store to buy some groceries. I bought carrots, apples, and bananas."
+        self.str_complex = """Natural language processing (NLP) is a subfield of linguistics, computer science,
+            and artificial intelligence concerned with the interactions between computers and human language,
+            in particular how to program computers to process and analyze large amounts of natural language data.
+            The goal is a computer capable of "understanding" the contents of documents, including the contextual
+            nuances of the language within them. The technology can then accurately extract information and insights
+            contained in the documents as well as categorize and organize the documents themselves.
+            """
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_remove_punctuation(self):
+        self.assertEqual(
+            Preprocessor.remove_punctuation(self.str_simple),
+            'Today I went to the store to buy some groceries I bought carrots apples and bananas'
+        )
+        self.assertEqual(
+            Preprocessor.remove_punctuation(self.str_complex),
+            """Natural language processing NLP is a subfield of linguistics computer science
+            and artificial intelligence concerned with the interactions between computers and human language
+            in particular how to program computers to process and analyze large amounts of natural language data
+            The goal is a computer capable of understanding the contents of documents including the contextual
+            nuances of the language within them The technology can then accurately extract information and insights
+            contained in the documents as well as categorize and organize the documents themselves
+            """
+        )
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_remove_stop_words(self):
+        self.assertEqual(
+            Preprocessor.remove_stop_words(self.str_simple),
+            ['Today', 'I', 'went', 'store', 'buy', 'groceries.', 'I', 'bought', 'carrots,', 'apples,', 'bananas.']
+        )
+        self.assertEqual(
+            Preprocessor.remove_stop_words(self.str_complex),
+            ['Natural', 'language', 'processing', '(NLP)', 'subfield', 'linguistics,', 'computer', 'science,',
+             'artificial', 'intelligence', 'concerned', 'interactions', 'computers', 'human', 'language,', 'particular',
+             'program', 'computers', 'process', 'analyze', 'large', 'amounts', 'natural', 'language', 'data.', 'The',
+             'goal', 'computer', 'capable', '"understanding"', 'contents', 'documents,', 'including', 'contextual',
+             'nuances', 'language', 'within', 'them.', 'The', 'technology', 'accurately', 'extract', 'information',
+             'insights', 'contained', 'documents', 'well', 'categorize', 'organize', 'documents', 'themselves.']
+        )
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_do_stem(self):
+        self.assertEqual(
+            Preprocessor.do_stem(self.str_simple.split(' ')),
+            ['today', 'i', 'went', 'to', 'the', 'store', 'to', 'buy', 'some', 'groceries.', 'i', 'bought', 'carrots,',
+             'apples,', 'and', 'bananas.']
+        )
+        self.assertEqual(
+            Preprocessor.do_stem(self.str_complex.split(' ')),
+            ['natur', 'languag', 'process', '(nlp)', 'is', 'a', 'subfield', 'of', 'linguistics,', 'comput', 'science,',
+             'and', 'artifici', 'intellig', 'concern', 'with', 'the', 'interact', 'between', 'comput', 'and', 'human',
+             'language,', 'in', 'particular', 'how', 'to', 'program', 'comput', 'to', 'process', 'and', 'analyz',
+             'larg','amount', 'of', 'natur', 'languag', 'data.', 'the', 'goal', 'is', 'a', 'comput', 'capabl', 'of',
+             '"understanding"', 'the', 'content', 'of', 'documents,', 'includ', 'the', 'contextu', 'nuanc', 'of', 'the',
+             'languag', 'within', 'them.', 'the', 'technolog', 'can', 'then', 'accur', 'extract', 'inform', 'and',
+             'insight', 'contain', 'in', 'the', 'document', 'as', 'well', 'as', 'categor', 'and', 'organ', 'the',
+             'document', 'themselves.']
+
+        )
 
 
 if __name__ == '__main__':
