3c6b8074e60baff6728b7f05b5a76a1750a48b42
Benjamin <bmk5670@psu.edu>
2022-12-05 20:15:29 -0500

    Updated

--- crawler/parse.py
+++ crawler/parse.py
@@ -2,17 +2,27 @@ from html.parser import HTMLParser
 import os.path
 
 
-class MyHTMLParser(HTMLParser):
+class NewHTMLParser(HTMLParser):
     text = ""
+
+    # def handle_starttag(self, tag, attrs):
+    #     if tag == "h3":
+    #         print("Start tag:", tag)
+
+    # def handle_endtag(self, tag):
+    #     if tag == "h3":
+    #         print("End tag:", tag)
 
     def handle_data(self, data):
         self.text += data.strip()
 
 
-parser = MyHTMLParser()
+output = ""
+parser = NewHTMLParser()
 d = os.getcwd() + r"\tutorial\patterns"
 for (path) in os.listdir(d):
     file = open(d + "\\" + path, "r", encoding='utf-8')
     parser.feed(file.read())
+    output += parser.text[parser.text.find("Intent"):parser.text.find("Support our free website and own the eBook!")] + '\n\n'
     file.close()
-print(parser.text)
+print(output)
