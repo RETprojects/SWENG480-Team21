7f62005666364f193658ebcd6d123882897e8a43
Benjamin <bmk5670@psu.edu>
2023-02-28 17:31:30 -0500

    Testing spider with user input start_urls and range

--- webserver/temp/forms.py
+++ webserver/temp/forms.py
@@ -1,13 +1,6 @@
 from django import forms
 
 
-class JonathanForm(forms.Form):
-    design_problem = forms.CharField(
-        label="",
-        widget=forms.Textarea(attrs={"cols": 40, "rows": 10}),
-    )
-
-
 class SubmitPatternForm(forms.Form):
     content = forms.CharField(label="", widget=forms.Textarea(attrs={}))
 
@@ -16,3 +9,4 @@ class CollectPatternForm(forms.Form):
     urlContent = forms.CharField(label="")
     sectionContent1 = forms.CharField(label="")
     sectionContent2 = forms.CharField(label="")
+
