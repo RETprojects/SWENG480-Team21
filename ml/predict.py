2541776f631f56fb495976994a9ac1ed8c4d51ad
remi <remitoutin@gmail.com>
2023-03-06 16:06:01 -0600

    Calculate RCD in predict.py

--- ml/predict.py
+++ ml/predict.py
@@ -174,24 +174,53 @@ def main():
     print("Running program")
     dp_1 = sys.argv[1]
 
-    df = pd.read_csv('sourcemaking.csv')
+    # df = pd.read_csv('sourcemaking.csv')
+    #
+    # corpus_with_dp = pd.concat([df['text'],pd.Series(dp_1)],ignore_index=True)
+    #
+    # # display(corpus_with_dp.iloc[-1])
+    #
+    # vect = TfidfVectorizer(min_df=1, stop_words="english")
+    # tfidf = vect.fit_transform(corpus_with_dp)
+    # pairwise_similarity = tfidf * tfidf.T
+    #
+    # cos_sim_dp1 = pairwise_similarity.toarray()[-1].tolist()
+    #
+    # df1 = pd.DataFrame(data={'pattern':df['pattern_name'],'cos_sim':cos_sim_dp1[:-1],'sorted_indices':np.argsort(np.argsort(cos_sim_dp1))[:-1]})
+    #
+    # # print(df1)
+    # from natsort import index_natsorted
+    #
+    # print(df1.sort_values(by='cos_sim',ascending=False)[:10].iloc[:,:2])
 
-    corpus_with_dp = pd.concat([df['text'],pd.Series(dp_1)],ignore_index=True)
+    language = 'english'
+    stemmer = PorterStemmer()
+    vectorizer = TfidfVectorizer(sublinear_tf=True)
+    df = getTrainData("GOF Patterns (2.0).csv")
 
-    # display(corpus_with_dp.iloc[-1])
+    # if (not validateInput(dp_1)):
+    #     print("Invalid input size! please try again. \n")
+    #     continue
 
-    vect = TfidfVectorizer(min_df=1, stop_words="english")                                                                                                                                                                                                   
-    tfidf = vect.fit_transform(corpus_with_dp)                                                                                                                                                                                                                       
-    pairwise_similarity = tfidf * tfidf.T 
+    problemRow = {'name': "design problem", 'correct_category': 4, 'overview': dp_1}
+    df = df.append(problemRow, ignore_index=True)
 
-    cos_sim_dp1 = pairwise_similarity.toarray()[-1].tolist()
+    corpus = df['overview'].tolist()
+    corpus = processCorpus(corpus, language, stemmer)
 
-    df1 = pd.DataFrame(data={'pattern':df['pattern_name'],'cos_sim':cos_sim_dp1[:-1],'sorted_indices':np.argsort(np.argsort(cos_sim_dp1))[:-1]})
+    X = vectorizer.fit_transform(corpus)
+    tf_idf = pd.DataFrame(data=X.toarray(), columns=vectorizer.get_feature_names_out())
 
-    # print(df1)
-    from natsort import index_natsorted
+    runAlgorithms(tf_idf, df)
 
-    print(df1.sort_values(by='cos_sim',ascending=False)[:10].iloc[:,:2])
+    cos_sim, txts = cosine_sim(df, df['overview'], df['Kmeans'].iloc[df.index[-1]], 1)
+    displayPredictions(cos_sim, txts, df)
+
+    # calculate and store the RCD
+    # RCD = number of right design patterns / total suggested design patterns
+    # This is a fraction of the suggested patterns that were in the correct cluster.
+    rcd = (len(txts.loc[df['Kmeans'] == df['correct_category']])) / (len(txts))
+    print("RCD = ", rcd)
 
 if __name__ == "__main__":
     main()
