3e718d8c9a88e8982780fe717a2684ae4ca5f157
bmk5670 <118693576+bmk5670@users.noreply.github.com>
2023-03-10 15:08:34 -0500

    Bmk5670 (#66)
    * Fixed html error.
    Included WIP files in changes.
    
    * Fixed html error.
    Included WIP files in changes.
    
    * Testing spider with user input start_urls and range
    
    * AutoSpider parse function updated to return text within specified range.
    Regex and conditionals added to determine if input is invalid
    Added database connector to output data
    
    * Preliminary work on connecting web scraping to website

--- webserver/temp/forms.py
+++ webserver/temp/forms.py
@@ -1,9 +1,12 @@
 from django import forms
-from django.forms import ModelForm
-from .models import PatternCatalog
 
 
-class PatternCatalogForm(ModelForm):
-    class Meta:
-        model = PatternCatalog
-        fields = ('name',)
+class SubmitPatternForm(forms.Form):
+    content = forms.CharField(label="", widget=forms.Textarea(attrs={}))
+
+
+class CollectPatternForm(forms.Form):
+    urlContent = forms.CharField(label="")
+    sectionContent1 = forms.CharField(label="")
+    sectionContent2 = forms.CharField(label="")
+
--- webserver/temp/views.py
+++ webserver/temp/views.py
@@ -1,10 +1,17 @@
-import json
+import os
+import sys
+
+from django.forms import Form
+
+sys.path.append('C:\\Users\\Benjamin\\PycharmProjects\\SWENG480-Team21')
+#import crawler.tutorial.tutorial.spiders.automated_scraping
 
 from django.core import serializers
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import PatternCategory, PatternCatalog, Pattern
+from .forms import SubmitPatternForm
 
 
 # Create your views here.
@@ -21,17 +28,29 @@ def browse_pattern(request):
     patternCategoryListJSON = serializers.get_serializer("json")().serialize(patternCategoryList)
     patternCatalogList = PatternCatalog.objects.all()
     patternCatalogListJSON = serializers.get_serializer("json")().serialize(patternCatalogList)
-    return render(request, 'browsepattern.html', {'patternList': patternList, 'patternCategoryList': patternCategoryList, 'patternCatalogList': patternCatalogList, 'patternListJSON': patternListJSON, 'patternCategoryListJSON': patternCategoryListJSON, 'patternCatalogListJSON': patternCatalogListJSON})
+    return render(request, 'browsepattern.html',
+                  {'patternList': patternList, 'patternCategoryList': patternCategoryList,
+                   'patternCatalogList': patternCatalogList, 'patternListJSON': patternListJSON,
+                   'patternCategoryListJSON': patternCategoryListJSON,
+                   'patternCatalogListJSON': patternCatalogListJSON})
+  
+
 def recommend_pattern(request):
     template = loader.get_template('recommendpattern.html')
     return HttpResponse(template.render())
 
 
 def submit_pattern(request):
-    template = loader.get_template('submitpattern.html')
-    return HttpResponse(template.render())
+    context = {
+        "form": SubmitPatternForm
+    }
+    return render(request, 'submitpattern.html', context=context)
 
 
 def collect_pattern(request):
-    template = loader.get_template('collectpattern.html')
-    return HttpResponse(template.render())
+    if request.method == 'POST' and 'run_script' in request.POST:
+        #print(sys.path)
+        from crawler.tutorial.tutorial.spiders.automated_scraping import run
+        run(1, 2)
+        #print(Form(request.POST).cleaned_data['textarea1'])
+    return render(request, 'collectpattern.html')
