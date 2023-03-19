a209b23e64a81a18da43ce2f86c056aa2abe963b
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2023-03-19 12:15:17 -0700

    Basic functional GUI

--- webserver/temp/urls.py
+++ webserver/temp/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('browsepattern/', views.browse_pattern, name='browse-pattern'),
-    path('recommendpattern/', views.recommend_pattern, name='recommend-pattern'),
-    path('submitpattern/', views.submit_pattern, name='submit-pattern'),
-    path('collectpattern/', views.collect_pattern, name='collect-pattern'),
+    path("jonathan/", views.jonathan),
+    path("home/", views.home, name="home"),
+    path("browsepattern/", views.browse_pattern, name="browse-pattern"),
+    path("recommendpattern/", views.recommend_pattern, name="recommend-pattern"),
+    path("submitpattern/", views.submit_pattern, name="submit-pattern"),
+    path("collectpattern/", views.collect_pattern, name="collect-pattern"),
 ]
