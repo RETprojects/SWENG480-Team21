db0aecf3f3ecbd8c7e689a3737f40da4d1141854
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2023-03-25 11:29:30 -0700

    Imported data working

--- webserver/pattern_pal/models.py
+++ webserver/pattern_pal/models.py
@@ -6,7 +6,13 @@ class MyPattern(models.Model):
     correct_category = models.IntegerField()
     overview = models.TextField()
 
+    class Meta:
+        db_table = "my_pattern"
+
 
 class MyProblem(models.Model):
     design_problem = models.TextField()
     correct_pattern = models.TextField()
+
+    class Meta:
+        db_table = "my_problem"
