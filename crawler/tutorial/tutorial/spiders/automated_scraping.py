8e1f7600b3752d10c20a8cd37c3757ea4ff348bf
Benjamin <bmk5670@psu.edu>
2023-04-12 16:06:15 -0400

    Testing with auto web cralwer

--- crawler/tutorial/tutorial/spiders/automated_scraping.py
+++ crawler/tutorial/tutorial/spiders/automated_scraping.py
@@ -1,5 +1,6 @@
 import scrapy
-import mysql.connector
+import sqlite3
+import os.path
 from bs4 import BeautifulSoup
 import re
 import os
@@ -13,14 +14,17 @@ class InvalidInputException(BaseException):
 
 def run(arg1, arg2):
     os.chdir(os.path.dirname(__file__))
-    print('scrapy crawl auto -a start_urls=' + arg1 + ' -a range=' + arg2)
-    print(type(arg2))
-    if arg2 == "":
-        os.system('scrapy crawl auto -a start_urls=' + arg1)
-    else:
-        os.system('scrapy crawl auto -a start_urls=' + arg1 + ' -a range="' + arg2 + '"')
+    os.system('scrapy crawl auto -a start_urls=' + arg1 + ' -a range="' + arg2 + '"')
+    #os.chdir(os.path.dirname(__file__))
+    #print('scrapy crawl auto -a start_urls=' + arg1 + ' -a range=' + arg2)
+    #print(type(arg2))
+    #if arg2 == "":
+    #    os.system('scrapy crawl auto -a start_urls=' + arg1)
+    #else:
+    #    os.system('scrapy crawl auto -a start_urls=' + arg1 + ' -a range="' + arg2 + '"')
     # os.system('scrapy crawl auto -a start_urls=https://www.crummy.com/software/BeautifulSoup/bs4/doc/ -a range="which have been removed.:+_:that no longer exists.:+_:Beautiful Soup will never be as fast as the parsers:+_:searching the document much faster."')
     # os.system('scrapy crawl auto -a start_urls=' + arg1 + ' -a range=' + arg2)
+
 
 class AutoSpider(scrapy.Spider):
     name = "auto"
@@ -41,9 +45,11 @@ class AutoSpider(scrapy.Spider):
         super().__init__(**kwargs)
 
     def parse(self, response, **kwargs):
-        db_connection = mysql.connector.connect(host="localhost", database="test", user="root", password="admin")
-        cursor = db_connection.cursor()
-        print("Connected to ", db_connection.get_server_info())
+        # print("TEST" + os.path.split(os.path.split(os.path.split(os.path.split(os.path.dirname(__file__))[0])[0])[0])[0] + "\webserver\sqlitedatabase.db")
+        conn = sqlite3.connect(
+            os.path.split(os.path.split(os.path.split(os.path.split(os.path.dirname(__file__))[0])[0])[0])[
+                0] + "\webserver\sqlitedatabase")
+        cur = conn.cursor()
         soup = BeautifulSoup(response.text, 'lxml')
         s = soup.get_text().strip()
         s = s.replace("\t", "").replace("\r", "").replace("\n", " ")
@@ -58,12 +64,12 @@ class AutoSpider(scrapy.Spider):
         for i in range(0, len(self.range), 2):
             text_in_range = s[s.find(self.range[i]):s.rfind(self.range[i + 1]) + len(self.range[i + 1])]
             # print(text_in_range)
-            add_pattern = ("INSERT INTO problem "
-                           "(category_id, description) "
-                           "VALUES (%s, %s)")
-            data_pattern = (1, text_in_range)
-            cursor.execute(add_pattern, data_pattern)
-            db_connection.commit()
+            # add_pattern = ("INSERT INTO problem "
+            #               "(category_id, description) "
+            #               "VALUES (?, ?)")
+            # data_pattern = (1, text_in_range)
+            # cur.execute(add_pattern, data_pattern)
+            # conn.commit()
 
     def errback(self, failure):
         self.logger.error(repr(failure))
--- webserver/temp/admin.py
+++ webserver/temp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Pattern, PatternCatalog, PatternCategory
+from .models import Pattern, PatternCatalog, PatternCategory, Problem
 
 # Register your models here.
 
 admin.site.register(Pattern)
 admin.site.register(PatternCatalog)
 admin.site.register(PatternCategory)
+admin.site.register(Problem)
