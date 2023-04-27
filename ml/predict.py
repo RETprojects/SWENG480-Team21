40aff770a869607d4d3426b66b2f8d95042f2496
remi <remitoutin@gmail.com>
2023-04-26 21:47:40 -0500

    Added more functions from PatternKMeans

--- ml/predict.py
+++ ml/predict.py
@@ -7,6 +7,8 @@ import nltk
 import numpy as np
 import pandas as pd
 from fcmeans import FCM
+from matplotlib import pyplot as plt
+from matplotlib import cm as cm
 from nltk import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.tag import pos_tag
@@ -15,7 +17,7 @@ from sklearn.cluster import AgglomerativeClustering, BisectingKMeans
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn_extra.cluster import KMedoids
-
+from sklearn.metrics import silhouette_samples, silhouette_score
 from wordcloud import WordCloud
 
 try:
@@ -81,6 +83,90 @@ def generateWordClouds(centroids):
         plt.imshow(wordcloud)
         plt.axis("off")
         plt.show()
+
+
+def run_KMeans(max_k, data):
+    max_k += 1
+    kmeans_results = dict()
+    for k in range(2, max_k):
+        kmeans = cluster.KMeans(
+            n_clusters=k,
+            init="k-means++",
+            n_init=10,
+            tol=0.0001
+            # , n_jobs = -1
+            ,
+            random_state=1,
+            algorithm="full",
+        )
+
+        kmeans_results.update({k: kmeans.fit(data)})
+
+    return kmeans_results
+
+
+def printAvg(avg_dict):
+    for avg in sorted(avg_dict.keys(), reverse=True):
+        print("Avg: {}\tK:{}".format(avg.round(4), avg_dict[avg]))
+
+
+def plotSilhouette(df, n_clusters, kmeans_labels, silhouette_avg):
+    fig, ax1 = plt.subplots(1)
+    fig.set_size_inches(8, 6)
+    ax1.set_xlim([-0.2, 1])
+    ax1.set_ylim([0, len(df) + (n_clusters + 1) * 10])
+
+    ax1.axvline(
+        x=silhouette_avg, color="red", linestyle="--"
+    )  # The vertical line for average silhouette score of all the values
+    ax1.set_yticks([])  # Clear the yaxis labels / ticks
+    ax1.set_xticks([-0.2, 0, 0.2, 0.4, 0.6, 0.8, 1])
+    plt.title(
+        ("Silhouette analysis for K = %d" % n_clusters), fontsize=10, fontweight="bold"
+    )
+
+    y_lower = 10
+    sample_silhouette_values = silhouette_samples(
+        df, kmeans_labels
+    )  # Compute the silhouette scores for each sample
+    for i in range(n_clusters):
+        ith_cluster_silhouette_values = sample_silhouette_values[kmeans_labels == i]
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.nipy_spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(
+            np.arange(y_lower, y_upper),
+            0,
+            ith_cluster_silhouette_values,
+            facecolor=color,
+            edgecolor=color,
+            alpha=0.7,
+        )
+
+        ax1.text(
+            -0.05, y_lower + 0.5 * size_cluster_i, str(i)
+        )  # Label the silhouette plots with their cluster numbers at the middle
+        y_lower = (
+            y_upper + 10
+        )  # Compute the new y_lower for next plot. 10 for the 0 samples
+    plt.show()
+
+
+def silhouette(kmeans_dict, df, plot=False):
+    df = df.to_numpy()
+    avg_dict = dict()
+    for n_clusters, kmeans in kmeans_dict.items():
+        kmeans_labels = kmeans.predict(df)
+        silhouette_avg = silhouette_score(
+            df, kmeans_labels
+        )  # Average Score for all Samples
+        avg_dict.update({silhouette_avg: n_clusters})
+
+        if plot:
+            plotSilhouette(df, n_clusters, kmeans_labels, silhouette_avg)
 
 
 def preprocess(series: pd.Series) -> pd.Series:
